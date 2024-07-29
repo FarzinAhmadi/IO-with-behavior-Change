644f5a135488de1d164ca556e914e0050bb1294c
Emmett Springer <espring6@jh.edu>
2024-07-29 11:49:08 -0400

    demo functions first commit

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='MfpDataLib',
+    packages=find_packages(),
+    # version='0.1.0',
+    description='Library of functions for processing, analyzing, and plotting MyFitnessPal data',
+    # install_requires=[]
+)
